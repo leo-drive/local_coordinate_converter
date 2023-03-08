35750896713f974c1a338576a92e0e32acea32af
meliketanrikulu <melike@leodrive.ai>
2023-03-08 17:22:15 +0300

    navsatfix part completed

--- include/local_coordinate_converter/local_coordinate_converter.hpp
+++ include/local_coordinate_converter/local_coordinate_converter.hpp
@@ -7,6 +7,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/nav_sat_fix.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <GeographicLib/UTMUPS.hpp>
+#include <autoware_sensing_msgs/msg/gnss_ins_orientation_stamped.hpp>
 #include <memory>
 
 class LocalCoordinateConverter : public rclcpp::Node {
@@ -14,10 +16,24 @@ public:
     LocalCoordinateConverter();
 
 
-    void NavSatFix2PoseWithCovarianceStamped(const sensor_msgs::msg::NavSatFix::ConstSharedPtr msg);
+    void NavSatFix2PoseWithCovarianceStamped(const sensor_msgs::msg::NavSatFix::ConstSharedPtr msg, int index);
 
-    std::vector<rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr> sub_vec;
+    std::vector<rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr> sub_navsatfix_vec;
+    std::vector<rclcpp::Subscription<autoware_sensing_msgs::msg::GnssInsOrientationStamped>::SharedPtr> sub_autoware_orientation_vec;
     std::vector<rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr> pub_vec;
+
+    rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr sub_sbg;
+    rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pub_sbg;
+
+    struct LocalOrigin {
+        double latitude;
+        double longitude;
+        double altitude;
+        double x;
+        double y;
+    };
+    LocalOrigin local_origin;
+    void LatLon2UTM(double lat, double lon, double &x, double &y);
 
 };
 
--- src/local_coordinate_converter.cpp
+++ src/local_coordinate_converter.cpp
@@ -2,31 +2,59 @@
 
 #include <memory>
 #include <local_coordinate_converter/local_coordinate_converter.hpp>
-
+#include <std_msgs/msg/string.hpp>
 #include <rclcpp/rclcpp.hpp>
-
 
 LocalCoordinateConverter::LocalCoordinateConverter()
         : Node("LocalCoordinateConverter") {
 
-    const auto number_of_input_topics = declare_parameter("number_of_input_topics", 0);
+    local_origin.latitude = declare_parameter("origin_latitude", 0.0);
+    local_origin.longitude = declare_parameter("origin_longitude", 0.0);
+    local_origin.altitude = declare_parameter("origin_altitude", 0.0);
+
+    LatLon2UTM(local_origin.latitude, local_origin.longitude, local_origin.x, local_origin.y);
+
+    const auto number_of_input_topics = declare_parameter("number_of_input_topics", 3);
 
     std::vector<std::string> topic_names_{};
     topic_names_ = (declare_parameter<std::vector<std::string>>("topic_names"));
+    sub_navsatfix_vec.reserve(number_of_input_topics);
 
-    sub_vec.reserve(number_of_input_topics);
+    for (int index = 0; index < topic_names_.size(); ++index) {
+        const auto topic_name = topic_names_[index];
 
-    for (const auto & topic_name : topic_names_ ) {
-        sub_vec.push_back(this->create_subscription<sensor_msgs::msg::NavSatFix>(
-                topic_name, 10, std::bind(&LocalCoordinateConverter::NavSatFix2PoseWithCovarianceStamped, this,  std::placeholders::_1)));
-        pub_vec.push_back(this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>(topic_name + "_local", 10));
+        std::function<void(const sensor_msgs::msg::NavSatFix::ConstSharedPtr msg)>  fcn
+            = std::bind(&LocalCoordinateConverter::NavSatFix2PoseWithCovarianceStamped,this, std::placeholders::_1, index);
+
+
+        sub_navsatfix_vec.push_back(this->create_subscription<sensor_msgs::msg::NavSatFix>(topic_name, 10, fcn));
+
+        pub_vec.push_back(this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>(topic_name + "_local_pose", 10));
+
     }
-}
 
-void LocalCoordinateConverter::NavSatFix2PoseWithCovarianceStamped(const sensor_msgs::msg::NavSatFix::ConstSharedPtr msg) {
-    RCLCPP_INFO(this->get_logger(), "I heard: [%f]", msg->latitude);
-    geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg_local = std::make_shared<geometry_msgs::msg::PoseWithCovarianceStamped>();
-    pub_vec[0]->publish(*msg_local);
+
+
+}
+void LocalCoordinateConverter::LatLon2UTM(double lat, double lon, double &x, double &y) {
+    int zone;
+    bool northp;
+    GeographicLib::UTMUPS::Forward(lat, lon, zone, northp, x, y);
+}
+void LocalCoordinateConverter::NavSatFix2PoseWithCovarianceStamped(const sensor_msgs::msg::NavSatFix::ConstSharedPtr msg, int index) {
+    geometry_msgs::msg::PoseWithCovarianceStamped pose_with_cov_msg;
+    pose_with_cov_msg.header = msg->header;
+    double x, y;
+    LatLon2UTM(msg->latitude, msg->longitude, x, y);
+    pose_with_cov_msg.pose.pose.position.x = x - local_origin.x;
+    pose_with_cov_msg.pose.pose.position.y = y - local_origin.y;
+    pose_with_cov_msg.pose.pose.position.z = msg->altitude - local_origin.altitude;
+
+    pose_with_cov_msg.pose.covariance[0] = msg->position_covariance[0];
+    pose_with_cov_msg.pose.covariance[4] = msg->position_covariance[4];
+    pose_with_cov_msg.pose.covariance[8] = msg->position_covariance[8];
+    pub_vec[index]->publish(pose_with_cov_msg);
+
 }
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
